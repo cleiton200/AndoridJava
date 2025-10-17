d345f0ca1bf8144a7ea5e28c2cb5ce59d3d707e3
Cleiton Gomes <gomescleiton420@gmail.com>
2025-10-17 14:37:12 -0300

    aula do dia 17/10 14:37

---
+++ secao7/src/exercicios.java
@@ -0,0 +1,30 @@
+public class exercicios {
+//exercicio 1
+//    public static void main(String[] args) {
+//        int i = 0;
+//        for (i = 0; i<= 50;i++){
+//            System.out.print(i+ " ");
+//        }
+//    }
+//   exercicio 2
+//    public static void main(String[] args) {
+//        int i = 50;
+//        for (i = 50; i >= 0;i--){
+//            System.out.print(i+ " ");
+//        }
+//    }
+// exercicio 3
+//    public static void main(String[] args) {
+//        int i = 0;
+//        for (i = 0; i<= 50;i++){
+//            if (i % 5 == 0){
+//                continue;
+//            }
+//            System.out.print(i+ " ");
+//        }
+//    }
+// exercicio 4
+
+
+
+}
