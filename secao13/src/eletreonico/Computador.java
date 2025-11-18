37e119c034560639d4c486de541f0818b8516a01
Cleiton Gomes <gomescleiton420@gmail.com>
2025-11-18 16:11:43 -0300

    aula dia 18/11/2025 16:11

--- secao13/src/eletreonico/Computador.java
+++ secao13/src/eletreonico/Computador.java
@@ -5,7 +5,6 @@ public class Computador implements Eletronico {
     //carregar os drives
     //carregar o SO
 
-    @Override
     public void ligar() {
         System.out.println("Carregar drivers");
         System.out.println("Carregar SO");
--- secao13/src/eletreonico/Microfone.java
+++ secao13/src/eletreonico/Microfone.java
@@ -1,8 +1,6 @@
 package eletreonico;
 
 public class Microfone implements Eletronico {
-
-    @Override
     public void ligar() {
         System.out.println("Ajusta os decibeis");
     }
