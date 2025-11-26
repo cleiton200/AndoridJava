3a8522152426680b9e12e66dbe3a085d4cb508b2
Cleiton Gomes <gomescleiton420@gmail.com>
2025-11-26 15:21:23 -0300

    aula dia 26/11/2025  15:21

---
+++ secao17/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //fazendo o uso do arquivo "data.csv"presente na pasta resoureces
+        //Exercicio: Imprime o total de vendas por vendedor
+        //Exercicio: Imprime o total de vendas por categoria
+
+    }
+}
